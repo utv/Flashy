92584cd0e49074491fff7f4fa21e03ce2f01999c
utv <k9amata@gmail.com>
2014-06-11 21:12:14 -0700

    update MainActivity because I forgot to do so. And clean up code.

--- Flashy/src/com/flashy/app/MainActivity.java
+++ Flashy/src/com/flashy/app/MainActivity.java
@@ -11,39 +11,21 @@ import it.gmariotti.cardslib.library.internal.CardGridArrayAdapter;
 import it.gmariotti.cardslib.library.view.CardGridView;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
-import android.text.InputType;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningServiceInfo;
-import android.app.AlertDialog;
+
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.ClipboardManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.ClipboardManager.OnPrimaryClipChangedListener;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteCursor;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
-import android.webkit.WebView;
-import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
-import android.widget.Toast;
-
-import com.memetix.mst.detect.Detect;
-import com.memetix.mst.language.Language;
-import com.memetix.mst.translate.Translate;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -52,13 +34,10 @@ public class MainActivity extends ActionBarActivity {
 	public SimpleCursorAdapter mAdapter;
 	public static CardGridArrayAdapter mCardArrayAdapter;
 	public static CardGridView gridView;
-	private String[] columns;
-	private int[] to;
+	//private String[] columns;
+	//private int[] to;
 	public static Cursor cursor;
 	private static ArrayList<Card> cards;
-	
-	// Array of languages for translation
-	private Language[] languages = Language.values();
 	
 	// clipboard service and its listener
 	private static ClipboardManager clipboard;
@@ -73,17 +52,12 @@ public class MainActivity extends ActionBarActivity {
                 	Log.i("performClipboardCheck", "item = " + cd.getItemAt(0).coerceToText(getApplicationContext()));
                 	
             		String word = cd.getItemAt(0).coerceToText(getApplicationContext()).toString();
-            		String meaning = "";
             		try {
             			// Translate a collected word and write to db.
                 		new Translation(helper, word);
 					} catch (Exception e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-            		
-            		//helper.insertCard(word, meaning);
-            		//helper.close();
             	} 
             } 
     }};
@@ -132,10 +106,7 @@ public class MainActivity extends ActionBarActivity {
 			startActivity(intent);
 		}
 		else if(id == R.id.action_importing){			
-			Log.i("onOptionsItemSelected", "action_importing");
 			
-			/*Intent intent = new Intent(this, CaptureWord.class);
-	        startActivity(intent);*/
 		}
 		return super.onOptionsItemSelected(item);
 	}
@@ -158,89 +129,9 @@ public class MainActivity extends ActionBarActivity {
 	protected void onResume() {
 		super.onResume();
 		Log.i("MainActivity::", "onResume");
-		getSupportFragmentManager().beginTransaction().replace(R.id.container, new PlaceholderFragment()).commit();
-		//mCardArrayAdapter.notifyDataSetChanged();
-		/*helper = new DBHelper(this);
-		cursor = helper.getAllByDefault();*//****//*
-		cards = new ArrayList<Card>();
-		mCardArrayAdapter = new CardGridArrayAdapter(this,cards);
-		
-		  
-        for(cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-        	FlashCard card = new FlashCard(this);
-        	final Long id = cursor.getLong(0);
-        	card.setId(id);
-        	
-        	final String term = cursor.getString(1);
-        	//Log.d("ID", String.valueOf(cursor.getInt(0)));
-        	card.setTerm(term);
-        	
-        	final String meaning = cursor.getString(2);
-        	//Log.d("meaning", meaning);
-        	card.setMeaning(meaning);
-        	
-        	if(cursor.getString(2).equals("")){
-        		card.setBackgroundResourceId(R.drawable.card_background_color1);
-        		
-        	}
-        	else
-        		card.setBackgroundResourceId(R.drawable.card_background_color2);
-    		
-        	card.setOnClickListener(new OnCardClickListener() {
-
-				@Override
-				public void onClick(Card card, View view) {
-					// TODO Auto-generated method stub
-					Intent n = null;
-					n = new Intent(getApplicationContext(), ScreenSlideActivity.class);
-					// Need - 1 for correct positions of cards.
-					n.putExtra("POSITION_KEY", Integer.parseInt(card.getId()) - 1);
-					//n.putExtra("DATA", data);
-					startActivity(n);
-				}
-        		
-        	});
-        		
-        	cards.add(card);
-        }
-		mCardArrayAdapter.notifyDataSetChanged();*/
-
+		getSupportFragmentManager().beginTransaction().
+			replace(R.id.container, new PlaceholderFragment()).commit();
 	}
-	
-	
-	/*
-	 * Unused for now.
-	 */
-	/*private void showWebViewDialog() {
-		// start webview
-		final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-		// Set up the input
-		final EditText input = new EditText(this);
-		input.setInputType(InputType.TYPE_CLASS_TEXT);
-		input.setOnEditorActionListener(new OnEditorActionListener() {
-			@Override
-			public boolean onEditorAction(TextView arg0, int actionId,
-					KeyEvent event) {
-				// TODO Auto-generated method stub
-				if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) 
-						|| (actionId == EditorInfo.IME_ACTION_DONE)) {
-					
-	                Log.i("onEditorAction","Enter pressed");
-	                // start webview activity
-	                Intent intent = new Intent(MainActivity.this, CaptureWord.class);
-	                intent.putExtra(CardEntry.ARG_URL, input.getText().toString().trim());
-	                startActivity(intent);
-	            }    
-	            
-				return false;
-			}
-	    });
-		alert.setView(input);
-		AlertDialog alertToShow = alert.create();
-		alertToShow.getWindow().setSoftInputMode(
-		    WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-		alertToShow.show();
-	}*/
 	
 	/**
 	 * A placeholder fragment containing a simple view.
@@ -322,14 +213,11 @@ public class MainActivity extends ActionBarActivity {
 					
 						card.setId(String.valueOf(id));
 						//SQLiteCursor data = (SQLiteCursor) mCardArrayAdapter.getItem(card.getId());
-
 						//data.moveToPosition(position);
 						
-						Toast.makeText(getActivity(),"Clickable card: pos "+card.getId(), Toast.LENGTH_LONG).show();
+						//Toast.makeText(getActivity(),"Clickable card: pos "+card.getId(), Toast.LENGTH_LONG).show();
 						Intent intent = new Intent(getActivity(), Form.class);
-						
 						intent.putExtra(CardEntry.COLUMN_NAME_CARD_ID, id);
-						
 						intent.putExtra(CardEntry.COLUMN_NAME_WORD, term);
 						intent.putExtra(CardEntry.COLUMN_NAME_MEANING, meaning);
 						intent.putExtra(CardEntry.MODE, CardEntry.EDIT);
@@ -340,52 +228,22 @@ public class MainActivity extends ActionBarActivity {
 	            });
 	        	
 	        	//set bg color to differentiate between card with both meaning and term or card with only term
-	        	if(cursor.getString(2).equals("")){
+	        	if(cursor.getString(2).equals(""))
 	        		card.setBackgroundResourceId(R.drawable.card_background_color1);
-	        		
-	        	}
 	        	else
 	        		card.setBackgroundResourceId(R.drawable.card_background_color2);
         		
-	        		
 	        	cards.add(card);
 	        	position++;
 	        }
+	        
             mCardArrayAdapter = new CardGridArrayAdapter(rootView.getContext(),cards);
             gridView = (CardGridView)rootView.findViewById(R.id.my_grid_list);
-            /*gridView.setOnItemLongClickListener(new OnItemLongClickListener() {
-
-				@Override
-				public boolean onItemLongClick(AdapterView<?> parent,
-						View v, int position, long id) {
-					// TODO Auto-generated method stub
-					//card.setId(String.valueOf(id));
-					//SQLiteCursor data = (SQLiteCursor) mCardArrayAdapter.getItem(position);
-					
-					data.moveToPosition(position);
-					
-					Toast.makeText(getActivity(),"Clickable card: pos "+card.getId(), Toast.LENGTH_LONG).show();
-					Intent intent = new Intent(getActivity(), Form.class);
-					
-					intent.putExtra(CardEntry.COLUMN_NAME_CARD_ID, id);
-					
-					intent.putExtra(CardEntry.COLUMN_NAME_WORD, term);
-					intent.putExtra(CardEntry.COLUMN_NAME_MEANING, meaning);
-					intent.putExtra(CardEntry.MODE, CardEntry.EDIT);
-					startActivity(intent);
-					return true;
-				
-				}
-            	
-            });*/
-            if (gridView!=null){
+            if (gridView!=null)
                 gridView.setAdapter(mCardArrayAdapter);
-            }
+            
             return rootView;
 		}
-		
 	}
-	/*
 	
-	*/
 }
